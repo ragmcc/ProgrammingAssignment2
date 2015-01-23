e820ed8f3033415e1a41a4be541b2c858d5095b1
Andres Gibson <andres@gibson.cl>
2015-01-23 00:15:46 -0300

    AGibson Assignment Commit
    Programming Assignment commit to be graded.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is Matrix constructor that can cache values
+## of complex operations done to it.
 
-## Write a short comment describing this function
-
+## Constructur function of the Matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    # Local value that hold the Inverse Matrix
+    inverse <- NULL
+    
+    # Update value of local Matrix and reset cache.
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    # Allow retrieve local matrix
+    get <- function() x
+    
+    # Update inverse value
+    setinverse <- function(inv) inverse <<- inv
+    # retrieve inverse matrix value
+    getinverse <- function() inverse
+    
+    # return new cMatrix as a list of functions. 
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Function responsible run Solve operation on 
+## a CacheMatrix object and update cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # check current value
+    inverse <- x$getinverse()
+    # if not null use cache 
+    if(!is.null(inverse)) {
+        message("getting cached data")
+        return(inverse)
+    }
+    # if no cache exist, run the operation
+    data <- x$get()
+    inverse <- solve(data, ...)
+    # update cache value
+    x$setinverse(inverse)
+    ## Return a matrix that is the inverse of 'x'
+    inverse    
 }
